ebe3a9481dd7bd6e086035237cfa0c248c700f7c
semkor <selsasss@gmail.com>
2021-12-07 20:33:16 +0200

    four commit

---
+++ src/lesson109/User.java
@@ -0,0 +1,13 @@
+package lesson109;
+
+public class User {
+    private long id;
+    private String name;
+    private String sessionId;
+
+    public User(long id, String name, String sessionId) {
+        this.id = id;
+        this.name = name;
+        this.sessionId = sessionId;
+    }
+}
