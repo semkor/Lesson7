18fd8763b503d1bdf0fb03b985762eecdd1d4fc6
semkor <selsasss@gmail.com>
2021-12-15 01:09:11 +0200

    lesson14

--- src/lesson18C/Solution.java
+++ src/lesson18C/Solution.java
@@ -5,27 +5,40 @@ import java.util.Arrays;
 public class Solution {
     public static void main(String[] args) {
 
-        System.out.println(Arrays.toString(findNumber("11")));
-        System.out.println(Arrays.toString(findNumber("125")));
-
-        System.out.println(Arrays.toString(findNumber(null)));
-        System.out.println(Arrays.toString(findNumber("11a")));
-        System.out.println(Arrays.toString(findNumber("aaaaa")));
-        System.out.println(Arrays.toString(findNumber("aaaaa  adfadfaf")));
+        System.out.println(Arrays.toString(findNumber("-12")));
+//        System.out.println(Arrays.toString(findNumber("125.0")));
+//        System.out.println(Arrays.toString(findNumber("0.57575")));
+//        System.out.println(Arrays.toString(findNumber("0000000000012")));
+//
+//        System.out.println(Arrays.toString(findNumber(null)));
+//        System.out.println(Arrays.toString(findNumber("11a")));
+//        System.out.println(Arrays.toString(findNumber("aaaaa")));
+//        System.out.println(Arrays.toString(findNumber("aaaaa  adfadfaf")));
     }
 
     public static int[] findNumber(String text) {
         int res;
         try {
-            res=Integer.parseInt(text);
+            res = Integer.parseInt(text);
         } catch (Exception e) {
-            System.out.print(text + " not a number");
+            System.out.println(text + " not a number");
             return null;
         }
 
-        int [] result=new int[res+1];
-        for(int i=0;i<result.length;i++){
-            result[i]=i;
+
+        if (res >= 0) {
+            int[] result = new int[res];
+            for (int i = 0; i < result.length; i++) {
+                result[i] = i;
+            }
+            return result;
+        } else {
+            int number=res*(-1);
+            int[] result = new int[number];
+            for (int i = 0; i < result.length; i++) {
+                result[i] = res++;
+            }
+            return result;
         }
-    return result;}
+    }
 }
