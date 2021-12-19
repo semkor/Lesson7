8ed0cba52ac20225303cf742afe3df0405e0026c
semkor <selsasss@gmail.com>
2021-12-19 02:07:04 +0200

    lesson14

---
+++ src/lesson28/Compar/isActiveComparator.java
@@ -0,0 +1,19 @@
+package lesson28.Compar;
+
+import lesson28.Comparator.Capability;
+
+import java.util.Comparator;
+
+public class isActiveComparator implements Comparator<Capability>{
+
+    @Override
+    public int compare(Capability o1, Capability o2) {
+        System.out.println("isActiveComparator is used");
+        if(o1.isActive() && !o2.isActive())
+            return -1;              //если true && false, тогда -1 - не меняем
+        else if((o1.isActive() && o2.isActive()) || (!o1.isActive() && !o2.isActive()))
+            return 0;               //если true && true или false && false - тогда 0 - оставляем
+        else
+            return 1;               //в других случаях меняем
+    }
+}
